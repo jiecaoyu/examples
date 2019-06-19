77ecd5520907a2ab363ce00386083580f9197982
Jiecao Yu <jiecaoyu@umich.edu>
2019-06-19 15:31:49 -0400

    lose the constrain to support normal training and eveluation

--- imagenet_admm/main.py
+++ imagenet_admm/main.py
@@ -218,7 +218,7 @@ def main_worker(gpu, ngpus_per_node, args):
         percentages = admm.gen_percentages(model, args.arch)
         admm_op = admm.retrain_op(model, percentages)
         admm_op.apply_mask()
-    else:
+    elif args.prune:
         raise Exception ("Please specify the prune stage: admm | retrain")
 
     # Data loading code
